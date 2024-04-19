9b4194219d93a89477c88177c250a9c819c8f261
a-l-r-e-n <a-l-r-e-n@mail.ru>
2024-04-19 22:28:44 +0700

    Добавлена библиотека Newtonsoft JSON.NET. Добавлен класс сериализации.

--- ContactsApp/ProjectManager.cs
+++ ContactsApp/ProjectManager.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.IO;
 using System.Xml;
 
@@ -6,10 +7,47 @@ using System.Xml;
 
 namespace ContactsApp
 {
+    /// <summary>
+    /// Менеджер для работы с файлами.
+    /// </summary>
     public class ProjectManager
     {
-        
+        /// <summary>
+        /// Путь к файлу.
+        /// </summary>
+        private const string _fileSave = "..\\My Documents\\ContactApp.notes";
+
+        /// <summary>
+        /// Сохранить контакт в файл.
+        /// </summary>
+        public static void SaveToFile(Contact data, string filename)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            using (StreamWriter sw = new StreamWriter(_fileSave + filename))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, data);
+            }
+        }
+
+        /// <summary>
+        /// Загрузить проект из файла.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public static Contact LoadFromFile(string filename)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            using (StreamReader sr = new StreamReader(_fileSave + filename))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                return (Contact)serializer.Deserialize<Contact>(reader);
+            }
+        }
+
 
     }
 
 }
+
+
